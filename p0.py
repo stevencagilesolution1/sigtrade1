c289e4301a504c39936b67765bb9fff9eb54fe57
stevencagilesolution1 <160616094+stevencagilesolution1@users.noreply.github.com>
2024-02-20 22:52:23 +0800

    Update p0.py

--- p0.py
+++ p0.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 url_param = st.query_params.to_dict()
-uid = url_param.get("uid", "")
-if len(uid):
-    st.markdown("Welcome<br>" + uid + "<br>Click <a href='http://43.135.26.66:8501/?uid=" + uid + "' target='_self'>here</a> to start", unsafe_allow_html=True)
+st.write(url_param)
